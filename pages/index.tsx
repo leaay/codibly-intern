b4030ec93678a5f6ac94df8cc831b1c3887e8f68
leaay <wiktorgs98@gmail.com>
2023-01-13 21:44:21 +0100

    filter and search input works

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.scss'
 import { useQuery } from 'react-query'
 import { useState } from 'react'
-
+import {} from '@mui/icons-material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SearchIcon from '@mui/icons-material/Search';
 
 export type product = {
   id: number,
@@ -19,15 +22,18 @@ export default function Home() {
 
 
     const [currentPage , setCurrentPage] = useState<number>(1)
+    const [searchInput , setSearchInput] = useState<string>('')
+    const [search, setSearch] = useState<string>('')
+    const [list , setList] = useState<product[]>()
 
 
-    const { data:products, isLoading, error , refetch , isFetching} = useQuery('posts', async ()=>{
-      const res = await fetch(`https://reqres.in/api/products${'?page=' + currentPage}&per_page=5`)
+    const { data:products, isLoading, error , refetch , isFetching} = useQuery([ search , currentPage], async ()=>{
+      const res = await fetch(`https://reqres.in/api/products?page=${currentPage}&per_page=5&id=${search}`)
       return res.json()
     },
     {
       refetchOnWindowFocus: false,
-      
+      onSuccess: ()=>setSearchInput(''),
     })
 
     function handlePage(page: number){
@@ -35,10 +41,20 @@ export default function Home() {
       refetch()
     }
 
+    function handleSearch(){
+      setSearch(searchInput)
+      refetch()
+      
+    }
+
   
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Something went wrong, please try agian later.</div>
-  console.log(products.data)
+  console.log(products)
+
+  
+
+
   return (
     <>
       <Head>
@@ -50,7 +66,10 @@ export default function Home() {
 
         <div className={styles.tableBody}>
 
-            <input type='number' />
+            <div className={styles.searchBox}>
+              <input disabled={isFetching} onChange={({target})=>{setSearchInput(String(target.value))}}   placeholder='search products' type='number' />
+              <button disabled={isFetching} className='btn' onClick={handleSearch} ><SearchIcon/></button>
+            </div>
 
             <div className={styles.productItem} style={{backgroundColor:`gray`, fontWeight:'bolder'}} >
               <p>id</p>
@@ -58,17 +77,48 @@ export default function Home() {
               <p>year</p>
             </div>
 
-            {products.data.map((product: product) => <div className={styles.productItem} style={{backgroundColor:`${product.color}7A`}} key={product.id}>
-              <p>{product.id}</p>
-              <p>{product.name}</p>
-              <p>{product.year}</p>
-            </div>  )}
+             
 
+            
+                {
+
+                !products.data ? 
+                
+                  <p>No results</p>:
+                
+                  search === '' ?
+                
+                      products.data.map((product: product)=> <div className={styles.productItem} style={{backgroundColor:`${product.color}7A`}} key={product.id}>
+                        <p>{product.id}</p>
+                        <p>{product.name}</p>
+                        <p>{product.year}</p>
+                      </div> ) : 
+
+                      <div className={styles.productItem} style={{backgroundColor:`${products?.data.color}7A`}} >
+                      <p>{products?.data.id}</p>
+                      <p>{products?.data.name}</p>
+                      <p>{products?.data.year}</p>
+                      </div>
+
+                }
+
+                
+
+
+
+
+
+            
+            {products.page ? 
             <div className={styles.pagination}>
-              <button onClick={()=>handlePage(-1)} disabled={isFetching} >Previous</button>
+              {currentPage !== 1 && <button className='btn' onClick={()=>handlePage(-1)} disabled={isFetching} ><ArrowBackIcon /></button> }
               <p>{currentPage}</p>
-              <button onClick={()=>handlePage(1)  } disabled={isFetching} >NExt</button>
-            </div>
+              {products.total_pages !== currentPage && <button className='btn' onClick={()=>handlePage(1)} disabled={isFetching} ><ArrowForwardIcon/></button> }
+            </div> : 
+            null
+            }
+
+
 
         </div>
 
@@ -78,3 +128,5 @@ export default function Home() {
 }
 
 
+// onChange={({target})=>{setSearch(String(target.value)) ; refetch()}}
+//onChange={({target})=>setSearch(target.value)}
