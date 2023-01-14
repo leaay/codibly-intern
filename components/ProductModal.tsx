b505f33559fd995a9cdb2ec7ab6c7e467ae01786
leaay <wiktorgs98@gmail.com>
2023-01-14 15:51:14 +0100

    fix

---
+++ components/ProductModal.tsx
@@ -0,0 +1,18 @@
+import { NextComponentType } from 'next'
+import React from 'react'
+
+
+
+const ProductModal:NextComponentType = () => {
+  return (
+
+    <div>
+        <div></div>
+    </div>
+
+  )
+}
+
+
+
+export default ProductModal
